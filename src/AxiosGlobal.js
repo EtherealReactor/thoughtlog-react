ed99bd7e58795be5259cd13675cc5647d3b32c4f
“rpr_maddi” <ram.prasad@introhive.com>
2018-03-03 21:57:58 +0530

    ThoughtlogReact: Backend URL fix.

--- src/AxiosGlobal.js
+++ src/AxiosGlobal.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 let instance = axios.create({
-  baseURL: 'http://localhost:10010/'
-  // baseURL: 'http://54.242.138.23/api/'
+  // baseURL: 'http://localhost:10010/'
+  baseURL: 'http://54.242.138.23/api/'
   
 });
 
